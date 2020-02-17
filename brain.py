0302426493865d43a4fcdc00095494c50a60bfe1
Richard Walmsley <r.walmsley97@gmail.com>
2020-02-17 17:19:59 -0330

    Initial basic model with multilayer perceptron.

--- brain.py
+++ brain.py
@@ -1,3 +1,20 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+
+#TODO::parsing data for inputs
+
+visible = keras.layers.Input(shape=(100,1))
+hidden1 = keras.layers.Dense(10, activation='relu')(visible)
+hidden2 = keras.layers.Dense(10, activation='relu')(hidden1)
+hidden3 = keras.layers.Dense(10, activation='relu')(hidden2)
+output = keras.layers.Dense(1, activation='sigmoid')(hidden3)
+model = keras.models.Model(inputs=visible, outputs=output)
+
+#TODO::training commands and saving the weight data
+
+# summarize layers
+print(model.summary())
+
+# plot graph
+plot_model(model, to_file='NNplot.png')
