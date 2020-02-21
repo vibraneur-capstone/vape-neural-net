a6ec43991c1d8c387a614df89c34bca23bf961e0
RichWalmsley <44400126+RichWalmsley@users.noreply.github.com>
2020-02-21 08:43:18 -0330

    Added spaceholder model.fit() with input and groundtruth parameters for when we actually set up our input data and ground truth.

--- brain.py
+++ brain.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-#TODO::parsing data for inputs
+#TODO::parsing/set up data for inputs
 
 # Inputs
 inputA = keras.layers.Input(shape=(4,))
@@ -10,14 +10,12 @@ inputB = keras.layers.Input(shape=(2140,))
 
 # First branch
 x = keras.layers.Dense(10, activation="relu")(inputA)
-#x = keras.models.Model(inputs = inputA, outputs = x)
 
 # Second branch
 y = keras.layers.Dense(500, activation='relu')(inputB)
 y = keras.layers.Dense(250, activation='relu')(y)
 y = keras.layers.Dense(100, activation='relu')(y)
 y = keras.layers.Dense(10, activation='relu')(y)
-#y = keras.models.Model(inputs = inputB, outputs = y)
 
 # Combine these branches
 concatenate = keras.layers.concatenate([x, y])
@@ -30,11 +28,14 @@ z = keras.layers.Dense(1, activation='sigmoid')(z)
 # Final model
 model = keras.models.Model(inputs=[inputA, inputB], outputs = z)
 
-# Create a model instance
-model = create_model()
-
 # Display model architecture
 model.summary()
 
-#TODO::training commands and saving the weighted data
+# Create a model instance
+model = create_model()
+
+#TODO:training
 model.compile(optimizer='adam', loss='crossentropy', metrics=['accuracy'])
+model.fit(input, groundtruth, epochs=, batch_size=)
+
+#TODO::checkpoint commands to save model, load in previous weights and continue training
