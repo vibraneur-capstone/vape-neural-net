911d9865ad6d428ff17cf3fdc3198f778954753b
RichWalmsley <44400126+RichWalmsley@users.noreply.github.com>
2020-03-07 18:23:07 -0330

    Added eval_gen to main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ modelname = 'model.h5'
 create = False
 load = True
 train = False
-evaluate = False
+evaluate = True
 predict = True
 ##
 
@@ -50,7 +50,7 @@ def getData(dataset, bearing):
 
 # Generate batches of a given size, stopping once it reaches the total amount of data
 # and streams this to the model iteratively as it trains.
-def generator(path, files, groundtruth, batchsize):
+def train_generator(path, files, groundtruth, batchsize):
     while True:
         # First for loop iterates across the whole dataset length in batch sizes
         for batch in range(0, len(files), batchsize):
@@ -82,6 +82,37 @@ def generator(path, files, groundtruth, batchsize):
                 # Stream this to the model during training
                 yield ([input1, input2], truth)
 
+def eval_generator(path, files, batchsize):
+    while True:
+        # First for loop iterates across the whole dataset length in batch sizes
+        for batch in range(0, len(files), batchsize):
+        
+            # Second for loop iterates through each batch
+            # so we yield 2 inputs and ground truth times the batch size
+            for i in range(batch, batch + batchsize):
+            
+                # Get file at specified index
+                f = files[i]
+            
+                # Open up our file
+                input = open(path + f,"r")
+                
+                # Read our file
+                line = np.array(input.readline().split(' '))
+                
+                # Split our input into an array of size (4,) and (10240,)
+                # and find corresponding truth value
+                split = np.split(line, [4])
+                try:
+                    input1 = np.array([split[0]]).astype(np.float32)
+                    input2 = np.array([split[1]]).astype(np.float32)
+                except:
+                    print("\nSomething went wrong with your data in file " + f + "\n")
+                    continue
+                
+                # Stream this to the model during training
+                yield ([input1, input2])
+
 ### Manipulate the model here ###
 
 if create:
@@ -101,7 +132,7 @@ if train:
         datapath, datalist, groundtruth, samples, batches, steps, epochs = getData(dataset, y) # dataset x, bearing y
 
         # Create generator instance
-        gen = generator(datapath, datalist, groundtruth, batches)
+        gen = train_generator(datapath, datalist, groundtruth, batches)
 
         # Train our model
         history = brain.TrainModel(m, modelname, gen, steps, epochs)
@@ -113,9 +144,9 @@ if train:
         datapath, datalist, groundtruth, samples, batches, steps, epochs = getData(dataset, y) # dataset x, bearing y
 
         # Create generator instance
-        gen = generator(datapath, datalist, groundtruth, batches)
+        gen = train_generator(datapath, datalist, groundtruth, batches)
 
-        # Train our msdodel
+        # Train our model
         history = brain.TrainModel(m, modelname, gen, steps, epochs)
 
 # Evaluation for dataset 2
@@ -123,7 +154,7 @@ if evaluate:
     for z in range(1,5):
         print("EVALUATING: Dataset %i, Bearing %i" % (2, z))
         
-        datapath, datalist, groundtruth, samples, batches, steps, epochs = getData(2, z) # dataset x, bearing y
+        datapath, datalist, groundtruth, samples, batches, steps, epochs = getData(2, z) # dataset 2, bearing y
 
         # Create generator instance
         gen = generator(datapath, datalist, groundtruth, batches)
@@ -133,14 +164,15 @@ if evaluate:
 
         print("Evaluation metrics: ", results)
 
+# Prediction for dataset 2
 if predict:
     for p in range(1,5):
         print("PREDICTING: Dataset %i, Bearing %i" % (2, p))
         
-        datapath, datalist, groundtruth, samples, batches, steps, epochs = getData(2, p) # dataset x, bearing y
+        datapath, datalist, groundtruth, samples, batches, steps, epochs = getData(2, p) # dataset 2, bearing p
 
         # Create generator instance
-        gen = generator(datapath, datalist, groundtruth, batches)
+        gen = eval_generator(datapath, datalist, batches)
 
         # Evaluate the model
         prediction = brain.PredictModel(m, modelname, gen, steps)
